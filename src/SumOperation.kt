130ef9d3b2797c8658c3166a0804fbcd3b654443
khaledhassssan <engkhaledhassan19@gmail.com>
2022-08-04 15:08:06 +0300

    The third version: three random numbers are added together and the result is printed

--- src/SumOperation.kt
+++ src/SumOperation.kt
@@ -1,13 +1,20 @@
 fun main() {
-    // Kotlin Program to Add Two Random Numbers
+    // Kotlin Program to Add three Random Numbers
+
     // First number
     var first = (1..10).shuffled().first();
     println("First number = "+first);
+
     // Second number
     var second = (1..10).shuffled().first();
     println("Second number = "+second);
+
+    // Third number
+    var third = (1..10).shuffled().first();
+    println("Third number = "+third);
+
     // Sum operation
-    var sum = first + second;
+    var sum = first + second +third;
     // Print result
-    println("Sum of Two Random Numbers is "+sum);
+    println("Sum of three Random Numbers is "+sum);
 }
