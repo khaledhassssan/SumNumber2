4b4a98715cb9cf5bd0b5c76d8a13240205da532b
khaledhassssan <engkhaledhassan19@gmail.com>
2022-08-04 14:26:00 +0300

    The first version: two fixed numbers are added together and the result is printed

---
+++ src/SumOperation.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // Kotlin Program to Add Two Numbers
+    // First number
+    var first = 10;
+    // Second number
+    var second = 20;
+    // Sum operation
+    var sum = first + second;
+    // Print result
+    println("Sum of Two Numbers is "+sum);
+}
